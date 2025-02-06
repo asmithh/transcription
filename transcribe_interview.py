64603f1c690cde5c628d16c1eb869d263e07e1ef
Alyssa <asmithh99@gmail.com>
2025-02-06 14:17:20 -0500

    making script more user-friendly

--- transcribe_interview.py
+++ transcribe_interview.py
@@ -1,18 +1,36 @@
+
+import argparse
+import yaml
+
+import librosa
+import numpy as np
+import soundfile as sf
+from transformers import pipeline
 from transformers.utils import logging
 logging.set_verbosity_error()
 
-import soundfile as sf
-import io
-import numpy as np
-import librosa
+parser = argparse.ArgumentParser(
+                    prog='transcription via distil-whisper',
+                    description='Transcribes a .mp3 file to a transcription w/ timestamps in .tsv format.',
+                    )
+parser.add_argument('-i', '--infile', help='the file (hopefully mp3) that you want to transcribe')
+parser.add_argument('-o', '--outfile', help='the filename of your output file (hopefully .tsv)')
+parser.add_argument('-c', '--config', help='the filepath of your configuration file; default is config.yml', default='config.yml')
 
-from transformers import pipeline
-import pickle
+args = parser.parse_args()
+INFILE = args.infile
+OUTFILE = args.outfile
+CONFIG_F = args.config
+try:
+    CONFIG = yaml.safe_load(open(CONFIG_F, 'r'))
+except Exception as e:
+    print(e)
+    raise ValueError('invalid YAML config file')
 
-INFILE = 'FILENAME.mp3'
-OUTFILE = 'TRANSCRIBED.tsv'
+MODEL = CONFIG['model']
+DEVICE = CONFIG['device']
 
-asr = pipeline("automatic-speech-recognition", model="distil-whisper/distil-medium.en", device=0)
+asr = pipeline("automatic-speech-recognition", model=MODEL, device=DEVICE)
 asr_sampling_rate = asr.feature_extractor.sampling_rate
 audio, sampling_rate = sf.read(INFILE)
 
